3421b699beb0e362fe4daf431ec3ae2c03dc862e
Jarva <4622609+Jarva@users.noreply.github.com>
2024-02-14 23:51:46 +0000

    fix: adjust item resolve order

--- src/main/java/com/github/jarva/arsadditions/util/PlayerInvUtil.java
+++ src/main/java/com/github/jarva/arsadditions/util/PlayerInvUtil.java
@@ -16,6 +16,10 @@ public class PlayerInvUtil {
     public static <T> T findItem(Player player, Predicate<ItemStack> is, T def, Function<ItemStack, T> map) {
         if(is.test(player.getMainHandItem()))return map.apply(player.getMainHandItem());
         if(is.test(player.getOffhandItem()))return map.apply(player.getOffhandItem());
+
+        List<SlotResult> s = CuriosApi.getCuriosHelper().findCurios(player, is);
+        if(!s.isEmpty()) return map.apply(s.get(0).stack());
+
         Inventory inv = player.getInventory();
         int size = inv.getContainerSize();
         for(int i = 0;i<size;i++) {
@@ -24,8 +28,7 @@ public class PlayerInvUtil {
                 return map.apply(s);
             }
         }
-        List<SlotResult> s = CuriosApi.getCuriosHelper().findCurios(player, is);
-        if(!s.isEmpty()) return map.apply(s.get(0).stack());
+
         return def;
     }
 }

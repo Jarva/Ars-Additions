78c75066813193e1a8a36c56a7b7e557a8981ea7
Jarva <4622609+Jarva@users.noreply.github.com>
2024-02-15 00:15:22 +0000

    fix: adjust item resolve order

--- src/main/java/com/github/jarva/arsadditions/util/PlayerInvUtil.java
+++ src/main/java/com/github/jarva/arsadditions/util/PlayerInvUtil.java
@@ -4,8 +4,10 @@ package com.github.jarva.arsadditions.util;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.util.LazyOptional;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotResult;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
 import java.util.List;
 import java.util.function.Function;
@@ -17,8 +19,11 @@ public class PlayerInvUtil {
         if(is.test(player.getMainHandItem()))return map.apply(player.getMainHandItem());
         if(is.test(player.getOffhandItem()))return map.apply(player.getOffhandItem());
 
-        List<SlotResult> s = CuriosApi.getCuriosHelper().findCurios(player, is);
-        if(!s.isEmpty()) return map.apply(s.get(0).stack());
+        LazyOptional<ICuriosItemHandler> opt = CuriosApi.getCuriosInventory(player);
+        if (opt.isPresent()) {
+            List<SlotResult> s = opt.resolve().get().findCurios(is);
+            if (!s.isEmpty()) return map.apply(s.get(0).stack());
+        }
 
         Inventory inv = player.getInventory();
         int size = inv.getContainerSize();
